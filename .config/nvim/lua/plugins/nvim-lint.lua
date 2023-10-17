3b2151843793f22cf5438570f089673c3f4b4c54
Stefan Subich <20615740+subich@users.noreply.github.com>
2023-10-17 11:55:55 -0400

    Update nvim-lint.lua

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,11 +1,16 @@
 return {
   "mfussenegger/nvim-lint",
   opts = {
+    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
     linters_by_ft = {
+      css = { "stylelint" },
       html = { "tidy" },
       javascript = { "eslint" },
       lua = { "selene" },
+      python = { "pylint" },
+      shell = { "shellcheck" },
       sql = { "sqlfluff" },
+      terraform = { "tfsec" },
       yaml = { "cfn_lint" },
     },
     linters = {
